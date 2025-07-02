362b27a01eaef45d4bee4340ce8ee847c07213c7
jinia101 <jiniasingha10@gmail.com>
2025-07-02 08:44:47 +0530

    feat: implement storage module with file upload functionality

--- apps/storage/src/main.ts
+++ apps/storage/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { StorageModule } from './storage.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { FILE_STORAGE_PACKAGE_NAME } from '@app/common';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(StorageModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    StorageModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: FILE_STORAGE_PACKAGE_NAME,
+        protoPath: join(__dirname, '../../../proto/file_storage.proto'),
+        url: `localhost:${process.env.PORT ?? 3000}`,
+      },
+    },
+  );
+
+  await app.listen();
+  console.log('File storage microservice is listening...');
 }
 bootstrap();
